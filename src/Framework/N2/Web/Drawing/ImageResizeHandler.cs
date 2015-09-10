49afb7fa055289fb14659ec12c2c6512b79273f9
Nimore <michael__h@hotmail.com>
2015-09-10 16:40:51 +0100

    Use built in MimeMapping.

--- src/Framework/N2/Web/Drawing/ImageResizeHandler.cs
+++ src/Framework/N2/Web/Drawing/ImageResizeHandler.cs
@@ -47,12 +47,12 @@ namespace N2.Web.Drawing
                 if (fs.FileExists(imageUrl))
                 {					
                     //string path = context.Server.MapPath(imageUrl);
-					if (CacheUtility.IsUnmodifiedSince(context.Request, fs.GetFile(imageUrl).Updated))
+                    if (CacheUtility.IsUnmodifiedSince(context.Request, fs.GetFile(imageUrl).Updated))
                     {
                         CacheUtility.NotModified(context.Response);
                     }
 
-                    context.Response.ContentType = GetContentType(imageUrl);
+                    context.Response.ContentType = MimeMapping.GetMimeMapping(imageUrl);
 
                     string extension = VirtualPathUtility.GetExtension(imageUrl);
                     ImageResizer ir = N2.Context.Current.Resolve<ImageResizer>();
@@ -69,23 +69,6 @@ namespace N2.Web.Drawing
                     throw new HttpException(404, "Not found");
                 }
             }            
-        }
-
-        private static string GetContentType(string filename)
-        {
-            var extension = Path.GetExtension(filename).ToLower();
-            switch (extension)
-            {
-                case ".gif":
-                    return "image/gif";
-                case ".png":
-                    return "image/png";
-                case ".jpg":
-                case ".jpeg":
-                    return "image/jpeg";				
-                default:
-                    return "application/x-" + extension.TrimStart('.');
-            }
         }
 
         public bool IsReusable
