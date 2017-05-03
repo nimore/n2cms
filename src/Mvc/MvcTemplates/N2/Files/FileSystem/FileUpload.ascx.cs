2aa8fa914e893d2c79c93b525f549d9410bcc3f5
Nimore <michael__h@hotmail.com>
2017-05-03 15:19:50 +0100

    Strip disallowed characters from uploaded file names.

--- src/Mvc/MvcTemplates/N2/Files/FileSystem/FileUpload.ascx.cs
+++ src/Mvc/MvcTemplates/N2/Files/FileSystem/FileUpload.ascx.cs
@@ -9,62 +9,64 @@ using System.Web.Configuration;
 using N2.Resources;
 using N2.Edit;
 using N2.Edit.FileSystem;
+using N2.Details;
+using System.Text.RegularExpressions;
 
 namespace N2.Management.Files.FileSystem
 {
-    public partial class FileUpload : System.Web.UI.UserControl
-    {
-        private readonly Engine.Logger<FileUpload> logger;
-        protected int maxFileSize = 4096 * 1024 - 10000;
-        protected SelectionUtility Selection { get; set; }
+	public partial class FileUpload : System.Web.UI.UserControl
+	{
+		protected int maxFileSize = 4096 * 1024 - 10000;
+		private readonly Engine.Logger<FileUpload> logger;
+		protected SelectionUtility Selection { get; set; }
 
-        protected override void OnInit(EventArgs e)
-        {
-            Selection = Page.GetSelection();
+		protected string GetLocalResourceString(string resourceKey, string defaultText = null)
+		{
+			try
+			{
+				return (string)GetLocalResourceObject(resourceKey);
+			}
+			catch (InvalidOperationException)
+			{
+				return defaultText;
+			}
+		}
 
-            base.OnInit(e);
+		protected void OnAlternativeCommand(object sender, CommandEventArgs args)
+		{
+			if (fuAlternative.PostedFile.ContentLength > 0)
+			{
+				string filename = Regex.Replace(System.IO.Path.GetFileName(fuAlternative.PostedFile.FileName), EditableFileUploadAttribute.InvalidCharactersExpression, "-");
+				string url = Selection.SelectedItem.Url.TrimEnd('/') + "/" + filename;
+				N2.Context.Current.Resolve<IFileSystem>().WriteFile(url, fuAlternative.PostedFile.InputStream);
+				Page.RefreshManagementInterface(Selection.SelectedItem);
+			}
+		}
 
-            InitMaxFileSize();
-            
-            Page.JQueryUi();
-        }
+		protected override void OnInit(EventArgs e)
+		{
+			Selection = Page.GetSelection();
 
-        private void InitMaxFileSize()
-        {
-            try
-            {
-                var config = ConfigurationManager.GetSection("system.web/httpRuntime") as HttpRuntimeSection;
-                maxFileSize = config.MaxRequestLength * 1024 - 10000;
-            }
-            catch (Exception ex)
-            {
-                logger.Error(ex);
-            }
-        }
+			base.OnInit(e);
 
-        // commands
+			InitMaxFileSize();
 
-        protected void OnAlternativeCommand(object sender, CommandEventArgs args)
-        {
-            if (fuAlternative.PostedFile.ContentLength > 0)
-            {
-                string filename = System.IO.Path.GetFileName(fuAlternative.PostedFile.FileName);
-                string url = Selection.SelectedItem.Url.TrimEnd('/') + "/" + filename;
-                N2.Context.Current.Resolve<IFileSystem>().WriteFile(url, fuAlternative.PostedFile.InputStream);
-                Page.RefreshManagementInterface(Selection.SelectedItem);
-            }
-        }
+			Page.JQueryUi();
+		}
 
-        protected string GetLocalResourceString(string resourceKey, string defaultText = null)
-        {
-            try
-            {
-                return (string)GetLocalResourceObject(resourceKey);
-            }
-            catch (InvalidOperationException)
-            {
-                return defaultText;
-            }
-        }
-    }
+		private void InitMaxFileSize()
+		{
+			try
+			{
+				var config = ConfigurationManager.GetSection("system.web/httpRuntime") as HttpRuntimeSection;
+				maxFileSize = config.MaxRequestLength * 1024 - 10000;
+			}
+			catch (Exception ex)
+			{
+				logger.Error(ex);
+			}
+		}
+
+		// commands
+	}
 }
