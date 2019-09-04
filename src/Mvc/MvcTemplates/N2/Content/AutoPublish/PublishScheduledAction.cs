40f46efd6e4461b5e031f487c15fea1a21e6f0e4
Mike Haworth <michael__h@hotmail.com>
2019-09-04 12:15:05 +0100

    Notify errors when publishing items on a schedule.

--- src/Mvc/MvcTemplates/N2/Content/AutoPublish/PublishScheduledAction.cs
+++ src/Mvc/MvcTemplates/N2/Content/AutoPublish/PublishScheduledAction.cs
@@ -8,6 +8,7 @@ using N2.Security;
 using N2.Edit.Workflow;
 using N2.Edit.Versioning;
 using N2.Engine;
+using N2.Web;
 
 namespace N2.Edit.AutoPublish
 {
@@ -15,19 +16,21 @@ namespace N2.Edit.AutoPublish
     [ScheduleExecution(30, TimeUnit.Seconds)]
     public class PublishScheduledAction : ScheduledAction
     {
-        private ContentVersionRepository versionRepository;
-        private IVersionManager versioner;
-        private IPersister persister;
-        private ISecurityManager security;
-        private StateChanger changer;
-        
-        public PublishScheduledAction(ContentVersionRepository versionRepository, IVersionManager versioner, IPersister persister, ISecurityManager security, StateChanger changer)
+        private readonly ContentVersionRepository versionRepository;
+        private readonly IVersionManager versioner;
+        private readonly IPersister persister;
+        private readonly ISecurityManager security;
+        private readonly StateChanger changer;
+        private readonly IErrorNotifier errorNotifier;
+
+        public PublishScheduledAction(ContentVersionRepository versionRepository, IVersionManager versioner, IPersister persister, ISecurityManager security, StateChanger changer, IErrorNotifier errorNotifier)
         {
             this.versionRepository = versionRepository;
             this.versioner = versioner;
             this.persister = persister;
             this.security = security;
             this.changer = changer;
+            this.errorNotifier = errorNotifier;
         }
 
         public virtual void MarkForFuturePublishing(ContentItem item, DateTime futureDate)
@@ -55,10 +58,17 @@ namespace N2.Edit.AutoPublish
                 .ToList();
             for (int i = 0; i < implicitExpire.Count; i++)
             {
-                // reset status on expired items
-                var item = implicitExpire[i];
-                changer.ChangeTo(item, ContentState.Unpublished);
-                persister.Save(item);
+                try
+                {
+                    // reset status on expired items
+                    var item = implicitExpire[i];
+                    changer.ChangeTo(item, ContentState.Unpublished);
+                    persister.Save(item);
+                }
+                catch(Exception e)
+                {
+                    errorNotifier.Notify(e);
+                }
             }
         }
 
@@ -70,10 +80,17 @@ namespace N2.Edit.AutoPublish
                 .ToList();
             for (int i = 0; i < implicitAutoPublish.Count; i++)
             {
-                // saving the master version for auto-publish will be eventually become published without this, but we want to update the state
-                var item = implicitAutoPublish[i];
-                changer.ChangeTo(item, ContentState.Published);
-                persister.Save(item);
+                try
+                {
+                    // saving the master version for auto-publish will be eventually become published without this, but we want to update the state
+                    var item = implicitAutoPublish[i];
+                    changer.ChangeTo(item, ContentState.Published);
+                    persister.Save(item);
+                }
+                catch(Exception e)
+                {
+                    errorNotifier.Notify(e);
+                }
             }
         }
 
@@ -82,9 +99,16 @@ namespace N2.Edit.AutoPublish
             var scheduledForAutoPublish = versionRepository.GetVersionsScheduledForPublish(Utility.CurrentTime()).ToList();
             foreach (var version in scheduledForAutoPublish)
             {
-                var scheduledVersion = versionRepository.DeserializeVersion(version);
-                scheduledVersion["FuturePublishDate"] = null;
-                versioner.Publish(persister, scheduledVersion);
+                try
+                {
+                    var scheduledVersion = versionRepository.DeserializeVersion(version);
+                    scheduledVersion["FuturePublishDate"] = null;
+                    versioner.Publish(persister, scheduledVersion);
+                }
+                catch(Exception e)
+                {
+                    errorNotifier.Notify(e);
+                }
             }
           }
     }
