f3acd25c5898466119fe197760c6d2a2daa7b278
Mike Haworth <michael__h@hotmail.com>
2019-09-04 16:11:11 +0100

    Further attempt to fix Versioning with nested children and EditableItem

--- src/Framework/N2/Edit/Workflow/Commands/SaveOnPageVersionCommand.cs
+++ src/Framework/N2/Edit/Workflow/Commands/SaveOnPageVersionCommand.cs
@@ -6,8 +6,8 @@ namespace N2.Edit.Workflow.Commands
 {
     public class SaveOnPageVersionCommand : CommandBase<CommandContext>
     {
-        private readonly IVersionManager versionMaker;
         private readonly IContentItemRepository itemRepository;
+        private readonly IVersionManager versionMaker;
 
         public SaveOnPageVersionCommand(IVersionManager versionMaker, IContentItemRepository itemRepository)
         {
@@ -24,44 +24,42 @@ namespace N2.Edit.Workflow.Commands
         {
             var item = state.Content;
             var page = Find.ClosestPage(item);
-            
+            var pageVersion = page.VersionOf.HasValue
+                ? page
+                : versionMaker.AddVersion(page, asPreviousVersion: false);
+
             if (!item.IsPage)
             {
-                var parentVersion = page.FindPartVersion(item.Parent);
+                var parentVersion = pageVersion.FindPartVersion(item.Parent);
 
                 if (state.Parameters.ContainsKey("MoveBeforeVersionKey") && !string.IsNullOrEmpty(state.Parameters["MoveBeforeVersionKey"] as string))
                 {
                     var beforeKey = (string)state.Parameters["MoveBeforeVersionKey"];
                     var beforeItem = page.FindDescendantByVersionKey(beforeKey);
                     beforeItem.Parent.InsertChildBefore(item, beforeItem.SortOrder);
-
                 }
                 else if (state.Parameters.ContainsKey("MoveBeforeSortOrder") && !string.IsNullOrEmpty(state.Parameters["MoveBeforeSortOrder"] as string))
                 {
                     int beforeSortOrder = Convert.ToInt32(state.Parameters["MoveBeforeSortOrder"]);
                     parentVersion.InsertChildBefore(item, beforeSortOrder);
                 }
-                else if (!parentVersion.Children.Contains(item))
+                else
                 {
-                    item.AddTo(parentVersion);
-                    Utility.UpdateSortOrder(parentVersion.Children);
+                    var itemVersion = pageVersion.FindPartVersion(item);
+                    if (!parentVersion.Children.Contains(itemVersion))
+                    {
+                        item.AddTo(parentVersion);
+                        Utility.UpdateSortOrder(parentVersion.Children);
+                    }
                 }
+                ////else if (item.ID == 0 && !parentVersion.Children.Contains(item))
+                ////{
+                ////    item.AddTo(parentVersion);
+                ////    Utility.UpdateSortOrder(parentVersion.Children);
+                ////}                
             }
 
-            var s = state.GetItemsToSave();
-
-            if (page.VersionOf.HasValue)
-            {
-                versionMaker.UpdateVersion(page);
-            }
-            else
-            {
-                versionMaker.AddVersion(page, asPreviousVersion: false);
-                if(item.ID == 0)
-                {
-                    itemRepository.SaveOrUpdate(item);
-                }
-            }
-        }
+            versionMaker.UpdateVersion(pageVersion);
+        }        
     }
 }
