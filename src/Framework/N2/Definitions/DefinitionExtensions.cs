f4628e347db3f123d95de622013cff5311ced1db
Mike Haworth <michael__h@hotmail.com>
2020-01-22 10:48:46 +0000

    Linq optimization

--- src/Framework/N2/Definitions/DefinitionExtensions.cs
+++ src/Framework/N2/Definitions/DefinitionExtensions.cs
@@ -1,20 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using N2.Security;
 using System.Security.Principal;
+using N2.Engine;
+using N2.Security;
 
 namespace N2.Definitions
 {
     public static class DefinitionExtensions
     {
-        public static IEnumerable<T> WhereAuthorized<T>(this IEnumerable<T> allSecurable, ISecurityManager security, IPrincipal user, ContentItem parentItem)
-            where T : ISecurableBase
-        {
-            return allSecurable.Where(s => security.IsAuthorized(s, user, parentItem));
-        }
-
         public static IEnumerable<ItemDefinition> AllowedBelow(this IEnumerable<ItemDefinition> allDefinitions, ItemDefinition parentDefinition, ContentItem parentItem, ContentItem childItem, IDefinitionManager definitions)
         {
             foreach (var definition in allDefinitions)
@@ -26,35 +20,13 @@ namespace N2.Definitions
 
         public static IEnumerable<TemplateDefinition> AllowedBelow(this IEnumerable<TemplateDefinition> allTemplates, ItemDefinition parentDefinition, ContentItem parentItem, IDefinitionManager definitions)
         {
-			if (allTemplates == null) yield break;
+            if (allTemplates == null) yield break;
 
             foreach (var template in allTemplates)
             {
                 if (IsAllowed(null, template.Definition, parentItem, parentDefinition, definitions))
                     yield return template;
             }
-        }
-
-		public static IEnumerable<TemplateDefinition> WhereAllowed(this IEnumerable<TemplateDefinition> allTemplates, ContentItem parentItem, string zoneName, IPrincipal user, IDefinitionManager definitions, ISecurityManager security)
-        {
-            return allTemplates.AllowedBelow(definitions.GetDefinition(parentItem), parentItem, definitions)
-                .Where(t => t.Definition.IsAllowedInZone(zoneName))
-                .Where(t => security.IsAuthorized(t.Definition, user, parentItem));
-        }
-
-        private static bool IsAllowed(ContentItem childItem, ItemDefinition childDefinition, ContentItem parentItem, ItemDefinition parentDefinition, IDefinitionManager definitions)
-        {
-            var ctx = new AllowedDefinitionQuery { Parent = parentItem, ParentDefinition = parentDefinition, Child = childItem, ChildDefinition = childDefinition, Definitions = definitions };
-            var filters = parentDefinition.AllowedChildFilters.Union(childDefinition.AllowedParentFilters).ToList();
-            if (filters.Any(f => f.IsAllowed(ctx) == AllowedDefinitionResult.Allow))
-                // filter specificly allows -> allow
-                return true;
-            else if (!filters.Any(f => f.IsAllowed(ctx) == AllowedDefinitionResult.Deny))
-                // no filter denies -> allow
-                return true;
-
-            // no filter allowed, but some filter denied -> deny
-            return false;
         }
 
         public static PropertyDefinition GetOrCreate(this IDictionary<string, PropertyDefinition> properties, string name, Type propertyType)
@@ -65,6 +37,27 @@ namespace N2.Definitions
             return property;
         }
 
+        public static bool IsThrowable(this ItemDefinition definition)
+        {
+            return definition.GetCustomAttributes<ThrowableAttribute>().Select(va => va.Throwable).FirstOrDefault() != AllowInTrash.No;
+        }
+
+        public static bool IsVersionable(this ItemDefinition definition)
+        {
+            return definition.GetCustomAttributes<VersionableAttribute>().Select(va => va.Versionable).FirstOrDefault() != AllowVersions.No;
+        }
+
+        public static IEnumerable<TemplateDefinition> WhereAllowed(this IEnumerable<TemplateDefinition> allTemplates, ContentItem parentItem, string zoneName, IPrincipal user, IDefinitionManager definitions, ISecurityManager security)
+        {
+            return allTemplates.AllowedBelow(definitions.GetDefinition(parentItem), parentItem, definitions)
+                .Where(t => t.Definition.IsAllowedInZone(zoneName) && security.IsAuthorized(t.Definition, user, parentItem));
+        }
+
+        public static IEnumerable<T> WhereAuthorized<T>(this IEnumerable<T> allSecurable, ISecurityManager security, IPrincipal user, ContentItem parentItem) where T : ISecurableBase
+        {
+            return allSecurable.Where(s => security.IsAuthorized(s, user, parentItem));
+        }
+
         internal static T TryClone<T>(this T obj)
         {
             if (obj is ICloneable)
@@ -73,14 +66,32 @@ namespace N2.Definitions
                 return obj;
         }
 
-        public static bool IsVersionable(this ItemDefinition definition)
+        private static bool IsAllowed(ContentItem childItem, ItemDefinition childDefinition, ContentItem parentItem, ItemDefinition parentDefinition, IDefinitionManager definitions)
         {
-            return definition.GetCustomAttributes<VersionableAttribute>().Select(va => va.Versionable).FirstOrDefault() != AllowVersions.No;
-        }
+            var ctx = new AllowedDefinitionQuery { Parent = parentItem, ParentDefinition = parentDefinition, Child = childItem, ChildDefinition = childDefinition, Definitions = definitions };
+            var filters = parentDefinition.AllowedChildFilters.Union(childDefinition.AllowedParentFilters).ToList();
+            try
+            {
+                if (filters.Any(f => f.IsAllowed(ctx) == AllowedDefinitionResult.Allow))
+                    // filter specificly allows -> allow
+                    return true;
+                else if (!filters.Any(f => f.IsAllowed(ctx) == AllowedDefinitionResult.Deny))
+                    // no filter denies -> allow
+                    return true;
 
-        public static bool IsThrowable(this ItemDefinition definition)
-        {
-            return definition.GetCustomAttributes<ThrowableAttribute>().Select(va => va.Throwable).FirstOrDefault() != AllowInTrash.No;
+                // no filter allowed, but some filter denied -> deny
+                return false;
+            }
+            catch(NullReferenceException)
+            {
+                Logger.ErrorFormat("IsAllowed > childItem: {0}", childItem);
+                Logger.ErrorFormat("IsAllowed > childDefinition: {0}", childDefinition);
+                Logger.ErrorFormat("IsAllowed > parentItem: {0}", parentItem);
+                Logger.ErrorFormat("IsAllowed > parentDefinition: {0}", parentDefinition);
+                Logger.ErrorFormat("IsAllowed > definitions: {0}", definitions);
+
+                throw;
+            }
         }
     }
 }
